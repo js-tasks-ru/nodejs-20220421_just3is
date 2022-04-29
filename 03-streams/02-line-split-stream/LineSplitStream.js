beab53d28b34c5cb57e6a55dfd51d80e6c4e91e4
just3is-dev <a.yalaev@icloud.com>
2022-04-29 18:39:56 +0300

    feature/03-line-split-stream: стрим, разбивающий текст на строки

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,32 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.ending = '';
   }
 
   _transform(chunk, encoding, callback) {
+    let data = chunk.toString();
+
+    if (data.includes(os.EOL)) {
+      data = this.ending + data;
+      this.ending = '';
+      data.split(os.EOL).forEach((line, index, lines) => {
+        const isLast = index === lines.length - 1;
+        if (isLast) {
+          this.ending = line;
+        } else {
+          this.push(line);
+        }
+      });
+    } else {
+      this.ending += data;
+    }
+
+    callback(null);
   }
 
   _flush(callback) {
+    callback(null, this.ending);
   }
 }
 
