027d3986c01e9da515a26c2f0c9faa4de4fe8781
just3is-dev <a.yalaev@icloud.com>
2022-05-03 17:42:50 +0300

    feature/03-file-server-delete: файловый сервер - удаление файла

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -10,9 +11,28 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.statusMessage = 'Not support symbol "/"';
+    return res.end();
+  }
+
   switch (req.method) {
     case 'DELETE':
-
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.statusMessage = 'File not found';
+          } else {
+            res.statusCode = 500;
+            res.statusMessage = 'Internal error';
+          }
+        } else {
+          res.statusCode = 200;
+        }
+        res.end();
+      });
       break;
 
     default:
