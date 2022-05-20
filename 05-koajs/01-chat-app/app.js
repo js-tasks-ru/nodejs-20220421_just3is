bf56f8fb732cd12efd6c41b09316b04613cff996
just3is-dev <a.yalaev@icloud.com>
2022-05-20 20:14:46 +0300

    feature/05-koajs-chat: чат на koa.js

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,31 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const subscribers = new Set();
+
 router.get('/subscribe', async (ctx, next) => {
+  const message = await new Promise((resolve, reject) => {
+    subscribers.add(resolve);
+
+    ctx.res.on('close', () => {
+      subscribers.delete(resolve);
+      resolve();
+    });
+  });
+
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+
+  if (!message) return ctx.throw(400, 'Missed or empty required field "message"');
+
+  subscribers.forEach((resolve) => resolve(message));
+
+  subscribers.clear();
+
+  ctx.body = 'Message have been published';
 });
 
 app.use(router.routes());
